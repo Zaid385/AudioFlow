7897aab07514b96b46e4bce51bc09f57ef1739b7
Zaid385 <zaidasaad385@gmail.com>
2026-07-11 06:49:08 +0500

    cleaned up junk files

--- Lab-FInal/server/src/routes/songRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as songController from '../controllers/songController';
-
-const router = Router();
-
-router.get('/', songController.getIndex);
-router.get('/songs', songController.getSongs);
-
-export default router;
